4028af1c29524b7781504fcd3502a8be651463dd
Ali Zein <alizein08042003@gmail.com>
2024-05-17 21:13:41 +0300

    Implemeted Authentication

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:projectt2/pages/home.dart';
+
+import 'pages/login.dart';
+import 'pages/signUp.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// ...
+
+
+
+
+
+Future<void> main() async  {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+   runApp(const MyApp());
+}
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _router = GoRouter(
+      initialLocation: '/signup', // Set initial route to login page
+      routes: [
+        GoRoute(
+          name: 'login',
+          path: '/login',
+          builder: (context, state) => Login(),
+        ),
+        GoRoute(
+          name: 'signup',
+          path: '/signup',
+          builder: (context, state) => SignUp(),
+        ),
+        GoRoute(
+          name: 'home',
+          path: '/',
+          builder: (context, state) => home(),
+        ),
+      ],
+    );
+    return MaterialApp.router(
+      routerConfig: _router,
+      title: 'Travel Gear',
+       
+    );
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to the Flutter homepage!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to the Flutter homepage!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
